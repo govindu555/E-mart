ede65f8a45852c41611a116d6587241966efa6bd
govind <govind@gmail.com>
2025-07-15 12:59:04 +0530

    modify

--- src/devices/index.js
+++ src/devices/index.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import { datadetails } from "../data";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './index.css'
 import { BsCart4 } from "react-icons/bs";
 import { useSelector } from "react-redux";
@@ -15,16 +15,16 @@ const Deviceitems=()=>{
 
     return(<>
     <nav>
-           <div className="search"><h1 className="logo">Sahasra</h1><input className="user" type="search" placeholder="Search Items" onChange={(e)=>setSearch(e.target.value)}></input><Link to="/card" className="l"><button className="cardlogo"><BsCart4 /> {items.length}</button></Link></div>
+           <div className="search"><h1 className="logo">Sahasra</h1><input className="user" type="search" placeholder="Search Items" onChange={(e)=>setSearch(e.target.value)}></input><NavLink to="/card" className="l"><button className="cardlogo"><BsCart4 /> {items.length}</button></NavLink></div>
           </nav>
           <div className="main2">
         {datadetails.filter(i=>i.name.includes(search)).map((each)=>{
             return(
-                <Link to={`/item/${each.id}`}><div className="devitem">
+                <NavLink to={`/item/${each.id}`}><div className="devitem">
                     <img className="imageitem" src={each.image} alt="image"/>
                     <p className="items">{each.model}</p>
                     <p className="items">{each.price}</p>
-                </div></Link>
+                </div></NavLink>
             )
         })}
     </div></>
